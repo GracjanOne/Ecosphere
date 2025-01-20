cd9068af3c65d38b036cec39ea27b63995890a56
unknown <gracjan.zalewski55@gmail.com>
2025-01-20 16:49:14 +0100

    Box with text and image component, new section margins

---
+++ src/app/components/BoxImage/BoxImage.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import "@/app/components/BoxImage/BoxImage.css"
+import Link from "next/link";
+export default function BoxImage() {
+  return (
+    <div className="web-margins box-wrapper">
+      <div className="image-box">
+        <Image src="/solar-panel.webp" alt="" width={550} height={730} />
+      </div>
+      <div className="text-box">
+        <h3 className="section-header">Przestań wydawać fortunę na rachunki</h3>
+        <div className="box-paragraphs">
+            <p>Jeśli opłaty zmuszają cię aby sięgać coraz głębiej do portfela, zaufaj najnowszej technologii energetycznej i zmniejsz rachunki za prąd nawet o połowę. Dzięki naszym panelom fotowoltaicznym, inwerterom, pompom ciepła czy systemom smart home, możesz znacznie obniżyć koszty utrzymania swojego domu, jednocześnie dbając o środowisko. </p>
+            <p>Nasze rozwiązania są nie tylko efektywne, ale także trwałe i łatwe w obsłudze. Zrób krok w kierunku przyszłości, oszczędzając pieniądze i korzystając z energii w bardziej zrównoważony sposób. Zacznij oszczędzać już dziś – inwestycja, która szybko się zwróci!</p>
+        </div>
+        <Link className="box-button" href="/kontakt">Zacznij oszczędzać</Link>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "./components/hero/HeroSection";
 import LeftImage from "./components/LeftImage/LeftImage";
+import BoxImage from "./components/BoxImage/BoxImage";
 export default function Home() {
   return (
    <div>
@@ -7,8 +8,7 @@ export default function Home() {
     <LeftImage title="Oszczędzaj nie tylko na prądzie" p1="Po instalacji naszych technologii czekają na ciebie również liczne zniżki podatkowe. Programy wsparcia dla odnawialnych źródeł energii, takie jak ulga termomodernizacyjna czy dopłaty rządowe, sprawiają, że instalacja naszych systemów staje się jeszcze bardziej opłacalna. " p2="Nie tylko zmniejszysz swoje rachunki, ale również odzyskasz część poniesionych wydatków dzięki preferencjom podatkowym. To podwójna korzyść, której nie warto przegapić!"
     button="Zobacz jakie zniżki możesz odebrać"
     img="/saving-img.webp" imgWidth={600} imgHeight={680} isRight={false}/>
-    <br /><br /><br /><br />
-    
+    <BoxImage />
    </div>
   );
 }
